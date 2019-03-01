85e9672245eaca78a75435e72cbe864cd480b506
Brainnotfound <kepo.steiner@yahoo.fr>
2019-03-01 09:46:06 +0100

    Premieres images du perso + premieresanimations

---
+++ New Unity Project/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    Animator anim;
+    int facing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        facing = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            facing = 2;
+
+        }
+        else
+        {
+            facing = 1;
+        }
+        anim.SetInteger("facing", facing);
+    }
+}
